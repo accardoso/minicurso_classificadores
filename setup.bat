b78f837d08fdf2ee3f0913ffd041e4fa2fd86c30
acardos5 <acardos5@its.jnj.com>
2019-11-07 12:05:33 -0300

    Replace setup.bat by the requirements.txt

--- setup.bat
+++
@@ -1,5 +0,0 @@
-::Run it from a anaconda prompt
-
-conda create -n ai_for_dev jupyter=1.0.0 numpy=1.17.3 scikit-learn=0.21.3 matplotlib=3.1.1 pandas=0.25.3 seaborn=0.9.0 nltk=3.4.4
-
-activate ai_for_dev
